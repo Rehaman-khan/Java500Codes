60a3d36fe088ca758454f757ef5e96f9beb77dc9
Rehaman-khan <rehamankhan1603@gmail.com>
2024-01-04 14:04:50 +0530

    1st Program

---
+++ SumofNumberinGivenRange.java
@@ -0,0 +1,11 @@
+public class SumofNumberinGivenRange {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 20;
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The Sum of  Natural Natural in given range are" + " " + sum);
+    }
+}
